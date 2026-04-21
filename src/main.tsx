81525ca86f23b4de44aa073d264611353a55d321
RubaeatFarhan <farhanrubaeat@gmail.com>
2026-04-21 10:28:23 +0600

    Fi

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/my-react-site">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
